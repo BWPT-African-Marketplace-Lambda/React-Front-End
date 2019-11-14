02685da53d9d7e04217b9bbe1a53f228fbcd9546
Joey Nevarez <josephnevarez12@gmail.com>
2019-11-13 19:07:02 -0800

    added BrowserRouter and am rendering (blank) NavBar, with Search component inside

---
+++ front-end/src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Search from './Search';
+
+function NavBar () {
+    return(
+        <div>
+            <Search/>
+        </div>
+    );
+};
+
+export default NavBar;
