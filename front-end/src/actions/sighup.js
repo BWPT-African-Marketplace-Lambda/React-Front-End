ef3ca649345ca7b176eab70b37b28f9bc7f7851d
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-11-14 16:48:15 -0600

    created actions for sighnup login and fetchItems

---
+++ front-end/src/actions/sighup.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+
+export const SIGHUP_START = "SIGNUP_START";
+export const SIGHUP_SUCCESS = "SIGNUP_SUCCESS";
+export const SIGHUP_ERROR = "SIGNUP_ERROR";
+
+export function signup(credentials) {
+  // this is our "thunk" function. redux-thunk middleware
+  // automatically gives us access to the dispatcher as a parameter
+  return dispatch => {
+    // we can kick off as many actions as we want,
+    // whenever we want. allows our action creator to be asyncronous.
+    dispatch({ type: SIGHUP_START });
+
+    axios
+      .post("", credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("user_id", res.data.new_user.id);
+      })
+      .catch(err =>{
+        dispatch({ type: SIGHUP_ERROR, payload: err.error })
+      })
+  }
+}
