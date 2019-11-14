148bc8d2229bf03fdddde10748a7effe83819803
Joey Nevarez <josephnevarez12@gmail.com>
2019-11-14 15:45:24 -0800

    fixed syntax error in App.js file

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import Search from './Search';
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
+//import Search from './Search';
 
 function NavBar () {
-    return(
-        <div>
-            <Search/>
-        </div>
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+    <div>
+        <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+            <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                <DropdownItem>
+                    Option 1
+                </DropdownItem>
+                <DropdownItem>
+                    Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                    Reset
+                </DropdownItem>
+                </DropdownMenu>
+            </UncontrolledDropdown>
+            </Nav>
+            </Collapse>
+        </Navbar>
+    </div>
     );
 };
 
+    
 export default NavBar;
