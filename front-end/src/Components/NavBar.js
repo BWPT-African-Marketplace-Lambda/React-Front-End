e1d0ea5ad01ce174cdf09dbf6e7c2c669f66c9ec
Joey Nevarez <josephnevarez12@gmail.com>
2019-11-14 17:37:25 -0800

    added new font

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 //import Search from './Search';
 
 const StyledNavTop = styled.div`
     background: #D79922;
+    font-family: 'Calistoga', cursive;
 `;
 
 const StyledNavBttm = styled.div`
     background: #EFE2BA;
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const StyledP = styled.p`
+    padding-left: 1%;
+    font-family: 'Calistoga', cursive;
 `;
 
 function NavBar () {
@@ -18,7 +27,9 @@ function NavBar () {
                 <h1>Sauti</h1>
             </StyledNavTop>
             <StyledNavBttm>
-                <a></a>
+                <Link to='/'><StyledP>Home</StyledP></Link>
+                <Link to='/'><StyledP>Log In</StyledP></Link>
+                <Link to='/'><StyledP>Sign Up</StyledP></Link>
             </StyledNavBttm>
         </div>
     );
