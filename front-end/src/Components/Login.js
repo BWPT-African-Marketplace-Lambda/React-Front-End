62224ff10c263848815331689de26d0fdf14db1b
tevin davis <aod.td1105@gmail.com>
2019-11-14 20:08:14 -0500

    Added login component and modified api component

---
+++ front-end/src/Components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Login() {
+    return (
+        <form onSubmit={}>
+            <input>
+            </input>
+            
+            <input>
+            </input>
+        </form>
+    )
+}
