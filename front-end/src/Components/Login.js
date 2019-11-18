58608093c23f86e72df47b2e2ff8b6ae734332bd
tevin davis <aod.td1105@gmail.com>
2019-11-17 20:52:49 -0500

    Added some functionality to signup and login components

--- front-end/src/Components/Login.js
+++ front-end/src/Components/Login.js
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
+import { connect } from 'react-redux';
+import { login } from '../actions/login';
+import api from '../utils/api';
+import { reducer } from '../reducers/login';
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_ERROR } from '../actions/login';
 
-export default function Login() {
+function Login(props) {
+    const [state, dispatch] = useReducer(reducer)
+    const [user, setUser] = useState({
+        email:"",
+        password:""
+    })
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        dispatch({type: LOGIN_SUCCESS, payload: user || props.user })
+
+        api()
+            .post("/auth/login", state)
+            .then(res => {
+                console.log(state)
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+
+    const handleChange = event => {
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value
+        })
+    }
+
     return (
-        <form onSubmit={}>
-            <input>
-            </input>
-            
-            <input>
-            </input>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type='text'
+                name='email'
+                placeholder='Email'
+                value={user.email}
+                onChange={handleChange}
+            />
+
+            <input 
+                type='text'
+                name='password'
+                placeholder='Password'
+                value={user.password}
+                onChange={handleChange}
+            />
+            <button type="submit">Login</button>
         </form>
     )
 }
+
+const mapStateToProps = state => {
+    return {
+        user: state.user.user
+    }
+}
+
+// const mapDispatchToProps = {
+//     login
+// }
+
+export default connect(mapStateToProps)(Login)
